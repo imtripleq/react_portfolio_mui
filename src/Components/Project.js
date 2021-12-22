42309c442561f33233fafe385f6cb71b3bce1b48
imtripleq <flamework89@hotmail.com>
2021-12-22 22:23:49 +1100

    working on portfolio

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -7,15 +7,35 @@ const Project = () => {
     page: {
       height: "100vh",
       backgroundColor: "white",
+      padding: "50px 100px",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      textAlign: "center",
     },
+    text: { width: "65%" },
+    list: { display: "flex" },
   });
 
+  const Card = () => <div> card</div>;
   const classes = useStyles();
   return (
     <>
       <Box component="div" className={classes.page}>
         <Box component="div" className={classes.text}>
           <Typography variant="h1">Hello World</Typography>
+          <Typography variant="h4" style={{ margin: "20px 0px" }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+            voluptatum veniam omnis quisquam assumenda corporis qui recusandae
+            enim iusto necessitatibus!
+          </Typography>
+        </Box>
+        <Box component="div" className={classes.list}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
         </Box>
       </Box>
     </>
